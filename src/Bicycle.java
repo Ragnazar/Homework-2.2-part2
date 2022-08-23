fe391a8414aa5868527245429f14df4287f950a2
Ragnazar <avdeyev.iv@gmail.com>
2022-08-23 11:22:21 +0300

    add-annotations

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -3,6 +3,7 @@ class Bicycle extends Vehicle {
     Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+    @Override
     public void checkEngine() {
         System.out.println("Проверяем электромотор");
     }
--- src/Truck.java
+++ src/Truck.java
@@ -2,6 +2,7 @@ class Truck extends Vehicle {
     Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+    @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку грузового автомобиля");
     }
